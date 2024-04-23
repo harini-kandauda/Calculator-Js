9e790de7a3932a8804dede305629d26e8c8ec24f
Harini Kandauda <101057843+harini-kandauda@users.noreply.github.com>
2024-04-23 11:24:50 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// Programme de la calculatrice
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input) {
+  display.value += input;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function calculate() {
+    try {
+    display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Erreur";
+    }
+}
